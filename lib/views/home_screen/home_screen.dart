f6174dba29531e22aa38a4c46b06ed81bac035ab
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2022-11-10 21:50:57 +0500

    Movies Neon Flutter App Setup

---
+++ lib/views/home_screen/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('Movies App')
+        ],
+      ),
+    );
+  }
+}
